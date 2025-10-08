e145590c57365c31d9fe35d5a0207527c80d8902
Fabio Chippari <chipparidev@gmail.com>
2025-10-08 18:38:37 -0300

    Completed Coffee Machine Project.

--- day-15-coffee-machine-project/coffee-machine-project.py
+++ day-15-coffee-machine-project/coffee-machine-project.py
@@ -34,12 +34,10 @@ resources = {
     "coffee": 100,
 }
 
-machine_money_earned = 0
-
 # > 2. Functions -------------------------------------------------------------------------------------------------------
 # >> 2.1. User Can Pay Function ----------------------------------------------------------------------------------------
 def user_can_pay(item_cost):
-    print(f"Price: ${item_cost}, please insert coins!")
+    print(f">> Price: ${item_cost}, please insert coins!")
     user_quarters = int(input("> How many quarters?: "))
     user_dimes = int(input("> How many dimes?: "))
     user_nickles = int(input("> How many nickles?: "))
@@ -69,54 +67,60 @@ def machine_can_make(item):
                 can_make = True
 
     if can_make:
-        return True, f"\nItem Chosen: {item}"
+        return True, f"\n>> Item Chosen: {item}"
     else:
-        return False, f"\nSorry, there is not enough {item_out_of_stock}!"
-
-# >> 2.3. Another Item Function ----------------------------------------------------------------------------------------
-def another_item():
-    another_choice = input("Would you like to purchase another item? (Y/N): ").upper()
-
-    if another_choice == "Y":
-        return True
-    else:
-        return False
+        return False, f">> Sorry, there is not enough {item_out_of_stock}!"
 
 # > 3. Main Code -------------------------------------------------------------------------------------------------------
 
-print("Welcome to our Coffee Machine!")
+def coffee_machine():
+    machine_money_earned = 0
+    print("Welcome to our Coffee Machine!")
 
-machine_loop = True
-while machine_loop:
-    user_choice = input("What would you like? (espresso/latte/cappuccino): ")
+    order = 0
+    machine_loop = True
+    while machine_loop:
+        if order == 0:
+            user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        else:
+            user_choice = input("\nWould you like anything else? Type (espresso/latte/cappuccino) "
+                                "or Type 'no' to finish your order: ").lower()
 
-    for key in MENU.keys():
-        if user_choice == key:
-            can_make_result = machine_can_make(user_choice)
-            can_pay_result = user_can_pay(MENU[user_choice]["cost"])
+        for key in MENU.keys():
+            if user_choice == key:
+                can_make_result = machine_can_make(user_choice)
 
-            if can_make_result[0]:
-                print(f"{can_make_result[1]}")
-                if can_pay_result[0]:
-                    print(can_pay_result[1])
-                    print(f"Here is your {user_choice}. Enjoy!")
-                    machine_money_earned = MENU[user_choice]["cost"]
+                if can_make_result[0]:
+                    print(can_make_result[1])
+                    can_pay_result = user_can_pay(MENU[user_choice]["cost"])
+                    if can_pay_result[0]:
+                        print(can_pay_result[1])
+                        print(f"Here is your {user_choice}. Enjoy!")
+                        machine_money_earned += MENU[user_choice]["cost"]
+                    else:
+                        print(can_pay_result[1])
                 else:
-                    print(can_pay_result[1])
-            else:
-                print(can_make_result[1])
+                    print(can_make_result[1])
 
-    if user_choice == "report":
-        print(">> Coffee Machine Report")
-        for key, value in resources.items():
-            if key == "water" or key == "milk":
-                print(f"{key.capitalize()}: {value}ml")
-            elif key == "coffee":
-                print(f"{key.capitalize()}: {value}g")
+        if user_choice == "report":
+            print(">> Coffee Machine Report")
+            for key, value in resources.items():
+                if key == "water" or key == "milk":
+                    print(f"{key.capitalize()}: {value}ml")
+                elif key == "coffee":
+                    print(f"{key.capitalize()}: {value}g")
 
-        print(f"Money: ${machine_money_earned}")
-    elif user_choice == "off":
-        print(">> The Coffee Machine is now off for maintainers!")
-        machine_loop = False
+            print(f"Money: ${machine_money_earned}")
 
+        if user_choice == "off":
+            print(">> The Coffee Machine is now off for maintainers!")
+            machine_loop = False
+
+        if user_choice == "no":
+            print("Thank you for choosing our Coffee Machine!")
+            machine_loop = False
+
+        order += 1
+
+coffee_machine()
 # ----------------------------------------------------------------------------------------------------------------------
