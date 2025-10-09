30cc6735ca326093a97ffb06ce9acd004ccdfa65
Fabio Chippari <chipparidev@gmail.com>
2025-10-09 12:25:47 -0300

    Update main.py

--- day-16/03-coffee-machine-v2/main.py
+++ day-16/03-coffee-machine-v2/main.py
@@ -1,6 +1,6 @@
 # > Coffee Machine Project v2 ------------------------------------------------------------------------------------------
 
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
