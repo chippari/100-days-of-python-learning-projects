97570233e633376fe7f2b5b4aa428981f85f431e
Fabio Chippari <chipparidev@gmail.com>
2025-10-09 10:30:30 -0300

    Learned about OOP and Turtle Lib.

---
+++ day-16/main.py
@@ -0,0 +1,17 @@
+
+# > Day 16 -------------------------------------------------------------------------------------------------------------
+# > 1. Learning about Object Oriented Programming ----------------------------------------------------------------------
+
+from turtle import Turtle, Screen
+
+bob = Turtle()
+bob.shape("turtle")
+bob.shapesize(2.0, 2.0, 1)
+bob.color("chartreuse")
+bob.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+# ----------------------------------------------------------------------------------------------------------------------
