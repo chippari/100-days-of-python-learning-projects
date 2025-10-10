d612f4c8e4f593fb138d939721a8057b5053a329
Fabio Chippari <chipparidev@gmail.com>
2025-10-10 12:01:18 -0300

    Learned about adding Methods to a Class.

--- day-17/main.py
+++ day-17/main.py
@@ -1,17 +1,30 @@
 
 # > Day 16 -------------------------------------------------------------------------------------------------------------
-# > 1. Learning about Constructors and __init()__ Function -------------------------------------------------------------
+# > 1. Learning about How to create your own Class ---------------------------------------------------------------------
 
 class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+# >> 1.1. Learning about Constructors and __init()__ Function ----------------------------------------------------------
 
 user_1 = User(101, "Fabio")
 user_2 = User(102, "Isabela")
 
 print(user_1.id, user_1.username, user_1.followers)
 print(user_2.id, user_2.username , user_2.followers)
+
+# >> 1.2. Learning about Adding Methods to a Class ----------------------------------------------------------------------
+
+user_1.follow(user_2)
+print(f"User_1 | Followers: {user_1.followers} | Following: {user_1.following}")
+print(f"User_ 2 | Followers: {user_2.followers} | Following: {user_2.following}")
 
 # ----------------------------------------------------------------------------------------------------------------------
