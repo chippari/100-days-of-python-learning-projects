23f7d6d1bfa434597d2c11bd6835cd2ff9093fb2
Fabio Chippari <chipparidev@gmail.com>
2025-10-10 13:20:10 -0300

    WIP: Quiz Project v1

---
+++ day-17/02-quiz-project/data.py
@@ -0,0 +1,17 @@
+# > Day 17 -------------------------------------------------------------------------------------------------------------
+# > 1. Creating the Question Data --------------------------------------------------------------------------------------
+
+question_data = [
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "Sharks are fish, not mammals.", "answer": "True"},
+    {"text": "Water boils at 100°C at sea level.", "answer": "True"},
+    {"text": "The Pacific Ocean is the largest ocean.", "answer": "True"},
+    {"text": "The human body has 206 bones.", "answer": "True"},
+    {"text": "The Sun is a planet.", "answer": "False"},
+    {"text": "The Eiffel Tower was meant to be temporary.", "answer": "True"},
+    {"text": "Dolphins are a type of large fish that breathe through gills.", "answer": "False"},
+    {"text": "The Moon produces its own light just like the Sun.", "answer": "False"}
+]
+
+# ----------------------------------------------------------------------------------------------------------------------
---
+++ day-17/02-quiz-project/main.py
@@ -0,0 +1,21 @@
+# > Day 17 -------------------------------------------------------------------------------------------------------------
+# > Quiz Game Project --------------------------------------------------------------------------------------------------
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print(f"You've completed the Quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
+
+# ----------------------------------------------------------------------------------------------------------------------
