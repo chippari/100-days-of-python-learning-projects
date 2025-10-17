98c38843f1482b71a63efde4b053fb83714ce810
Fabio Chippari <chipparidev@gmail.com>
2025-10-17 10:26:15 -0300

    feat: learned about open, read and write in a file.

--- day-24/02-learning-to-use-files/main.py
+++ day-24/02-learning-to-use-files/main.py
@@ -1,14 +1,41 @@
 # > Day 24 -------------------------------------------------------------------------------------------------------------
 # > 1. Learning about How to Open, Read and Write Files ----------------------------------------------------------------
 
-# Open the file and Set it in a variable.
-file = open('my_file.txt')
+# >> 1.1. Not Recommend Practice ---------------------------------------------------------------------------------------
 
-# Read the file and set it in a variable.
-contents = file.read()
-print(contents)
+# Open File and Set it in a variable.
+# file = open('my_file.txt')
 
-# After open and use it, it's necessary to close the file.
-file.close()
+# Read File and set it in a variable.
+# contents = file.read()
+# print(contents)
+
+# After open and use it, it's necessary to close File.
+# file.close()
+
+# >> 1.2. Best Practice ------------------------------------------------------------------------------------------------
+
+# Use like this, because you don't need to close the file afterward.
+# Open File and save it in a variable.
+
+# with open('my_file.txt') as file:
+#     # Read File.
+#     contents = file.read()
+#     print(contents)
+
+# >> 1.3. Writing in a File --------------------------------------------------------------------------------------------
+
+# Open File and set it a Writing Mode = 'w', in which delete all past content and write the new message.
+# ATTENTION: if it doesn't exist the file, writing mode will create a file for you.
+
+# with open('my_file.txt', mode='w') as file:
+#     file.write('Hello World!')
+
+# >> 1.4. Adding new line of text in a File ----------------------------------------------------------------------------
+
+# ATTENTION: Every time your run the code, it will write it again.
+
+# with open('my_file.txt', 'a') as file:
+#     file.write('\nHello World!')
 
 # ----------------------------------------------------------------------------------------------------------------------
