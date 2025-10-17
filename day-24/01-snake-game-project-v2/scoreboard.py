1f3a18ffd67c545415937f86cb77131749fc2f6d
Fabio Chippari <chipparidev@gmail.com>
2025-10-17 11:05:51 -0300

    feat: add highscore_data.txt to track it.

--- day-24/01-snake-game-project-v2/scoreboard.py
+++ day-24/01-snake-game-project-v2/scoreboard.py
@@ -3,23 +3,27 @@
 
 from turtle import Turtle
 
+# CONFIGURATION (Constant Scoreboard Variables)
 ALIGNMENT = 'center'
 FONT = ("Courier", 14, "bold")
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.speed("fastest")
         self.color("white")
         self.goto(0, 275)
+        self.score = 0
+        with open(file='highscore_data.txt', mode='r') as highscore_data:
+            self.high_score = int(highscore_data.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
+        with open(file='highscore_data.txt', mode='r') as highscore_data:
+            self.high_score = int(highscore_data.read())
         self.write(arg=f"| Score: {self.score} | High Score: {self.high_score} |", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
@@ -29,6 +33,8 @@ class ScoreBoard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open(file='highscore_data.txt', mode='w') as highscore_data:
+                highscore_data.write(f"{self.high_score}")
 
         self.score = 0
         self.update_scoreboard()
