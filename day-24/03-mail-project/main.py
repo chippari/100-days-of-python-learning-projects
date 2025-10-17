26065fc4c30fbe39342301072bb34bb460c85751
Fabio Chippari <chipparidev@gmail.com>
2025-10-17 12:12:30 -0300

    init: mail project.

---
+++ day-24/03-mail-project/main.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 24 - Mail Project ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: main.py
+# > Module: Main Module
+# > Description: Entry point of the application; initializes and runs the main logic.
+# > Author: Fabio Chippari
+# > Created: 2025-10-17
+# ----------------------------------------------------------------------------------------------------------------------
+
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
