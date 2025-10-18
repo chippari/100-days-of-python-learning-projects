4975309c1b17e9c5b6964e58d8c37166f1b6ab83
Fabio Chippari <chipparidev@gmail.com>
2025-10-18 17:36:12 -0300

    init: exercise 3 about list comprehension.

---
+++ day-26/01-list-comprehension/exercise_3.py
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 26 - List Comprehension Exercise 3 -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: exercise_3.py
+# > Module: Exercise 3 Module
+# > Description: Exercise 3 about List Comprehension.
+# > Author: Fabio Chippari
+# > Created: 2025-10-18
+# ----------------------------------------------------------------------------------------------------------------------
+# > [Exercise 3] Question ----------------------------------------------------------------------------------------------
+
+# TODO: Data Overlap
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+# You are going to create a list called result which contains the numbers that are common in both files.
+# e.g. if file1.txt contained:
+# 1
+# 2
+# 3
+#
+# and file2.txt contained:
+# 2
+# 3
+# 4
+#
+# result = [2, 3]
+# IMPORTANT:  The output should be a list of integers and not strings!
+# Try to use List Comprehension instead of a Loop.
+
+# > [Exercise 3] Solution ----------------------------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
