a30ead5fd6cb452e0791c5e4afbbe168bfe2de6e
Fabio Chippari <chipparidev@gmail.com>
2025-10-18 15:53:41 -0300

    release: U.S. States Game Project Completed.

--- day-25/03-us-states-game-project/main.py
+++ day-25/03-us-states-game-project/main.py
@@ -30,7 +30,8 @@ text.color("black")
 states_data = pandas.read_csv("50_states.csv")
 
 # States Guessed Variable.
-states_guessed = 0
+states_guessed = []
+states_guessed_count = 0
 
 game_is_on = True
 while game_is_on:
@@ -38,7 +39,7 @@ while game_is_on:
     screen.update()
 
     # Display a Window to ask and Store it in a Variable.
-    user_guess = screen.textinput(title=f"Guess the States {states_guessed}/50", prompt="What's another state's name?").title()
+    user_guess = screen.textinput(title=f"Guess the States {states_guessed_count}/50", prompt="What's another state's name?").title()
 
     # States Guess Loop, if is correct show on Screen and increase states_guessed.
     for state_name in states_data.state:
@@ -50,9 +51,33 @@ while game_is_on:
             text.goto(pos_x, pos_y)
             text.write(f"{name}", align="center", font=("Arial", 8, "bold"))
 
-            states_guessed += 1
+            states_guessed.append(state_name)
+            states_guessed_count += 1
 
-    if states_guessed == 50:
+    if user_guess == "Exit":
         game_is_on = False
+
+    if states_guessed_count == 50:
+        game_is_on = False
+
+# If User didn't guess all 50 states.
+if states_guessed_count != 50:
+    # Create CSV File to Show States that weren't guessed by User.
+    # 1. Create States Not Guessed List.
+    states_not_guessed = []
+
+    # 2. Loop trough States Name in States Data, and if it weren't guessed by User, append to States Not Guessed List.
+    for state_name in states_data.state:
+        if state_name not in states_guessed:
+            states_not_guessed.append(state_name)
+
+    # 3. Create States Not Guessed Dictionary with States Names that weren't guessed.
+    states_not_guessed_dict = {
+        "State": states_not_guessed
+    }
+
+    # 4. Create a States Data Frame with States Not Guessed Dictionary and Convert it to a new CSV File.
+    states_df = pandas.DataFrame(states_not_guessed_dict)
+    states_df.to_csv("states_not_guessed.csv")
 
 # ----------------------------------------------------------------------------------------------------------------------
