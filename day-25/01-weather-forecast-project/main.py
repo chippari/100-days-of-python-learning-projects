538ad09245267330677534dc9129775cc3ada80f
Fabio Chippari <chipparidev@gmail.com>
2025-10-18 03:19:09 -0300

    feat: learned a lot about Pandas library and How to manipulate data with it.

--- day-25/01-weather-forecast-project/main.py
+++ day-25/01-weather-forecast-project/main.py
@@ -32,7 +32,60 @@ import pandas
 # Get and Read CSV File using pandas.
 weather_data = pandas.read_csv("weather_data.csv")
 
-# Print Specific Column, like "temp"
-print(weather_data["temp"])
+# Print Specific Column, like "temp".
+# print(weather_data["temp"])
+
+# The Most Two Important Data Structure.
+# 1. Dataframe (2-dimensional labeled data structure):
+# print(type(weather_data))
+
+# 2. Series (one-dimensional labeled array):
+# print(type(weather_data["temp"]))
+
+# Convert CSV to a Dictionary.
+# weather_dict = weather_data.to_dict()
+# print(weather_dict)
+
+# Convert CSV to a List.
+# weather_list = weather_data["temp"].to_list()
+# print(weather_list)
+
+# Average Week Temperature.
+# average_temp = round(weather_data["temp"].mean())
+# print(f"Average Week Temperature is: {average_temp}")
+
+# Maximum Week Temperature.
+# max_temp = weather_data["temp"].max()
+# print(f"Max Week Temperature is: {max_temp}")
+
+# Exist Two Ways to Get Data in Columns.
+# 1. Using like a dictionary by [""]
+# print(weather_data["temp"])
+# 2. Using like an attribute
+# print(weather_data.temp)
+
+# Get Data in Row.
+# monday_data = weather_data[weather_data.day == "Monday"]
+# print(monday_data)
+
+# Get Row of Maximum Week Temperature.
+# max_temp_data = weather_data[weather_data.temp == weather_data.temp.max()]
+# print(max_temp_data)
+
+# Convert Monday Temperature from Celsius to Fahrenheit.
+# monday_data = weather_data[weather_data.day == 'Monday']
+# monday_temp = monday_data.temp[0]
+# monday_temp_fahr = monday_temp * 9/5 + 32
+# print(monday_temp_fahr)
+
+# Create Dataframe from Scratch.
+# Example of Dictionary:
+# student_dict = {
+#     "student": ["Fabio", "Isabela", "Alex"],
+#     "scores": [76, 88, 57]
+# }
+#
+# student_data = pandas.DataFrame(student_dict)
+# print(student_data)
 
 # ----------------------------------------------------------------------------------------------------------------------
