192d6f665e4a08a8d4b4bacb702160780c5b89e9
Fabio Chippari <chipparidev@gmail.com>
2025-10-18 17:22:04 -0300

    init: Exercise 1 about list comprehension.

---
+++ day-26/01-list-comprehension/exercise_1.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 26 - List Comprehension Exercise 1 -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: exercise_1.py
+# > Module: Exercise 1 Module
+# > Description: Exercise 1 about List Comprehension.
+# > Author: Fabio Chippari
+# > Created: 2025-10-18
+# ----------------------------------------------------------------------------------------------------------------------
+# > [Exercise 1] Question ----------------------------------------------------------------------------------------------
+
+# TODO: Squaring Numbers
+# You are going to write a List Comprehension to create a new list called squared_numbers. This new list should contain
+# every number in the list numbers but each number should be squared.
+
+# E.g: 4 * 4 = 16 -> 4 squared equals 16.
+# [DO NOT] modify the List numbers directly. Try to use List Comprehension instead of a Loop.
+
+# Target Output:
+# [1, 1, 4, 9, 25, 64, 169, 441, 1156, 3025]
+
+# > [Exercise 1] Solution ----------------------------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
