9e768a8af64dede5198757f99212ab2b9248cd3d
Fabio Chippari <chipparidev@gmail.com>
2025-10-18 12:41:56 -0300

    release: Count each color and afterwards create a new CSV file.

--- day-25/02-squirrel-project/main.py
+++ day-25/02-squirrel-project/main.py
@@ -9,5 +9,24 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 import pandas
+from numpy.matlib import empty
+
+# Get Squirrel Data by Reading its CSV File.
+squirrel_data = pandas.read_csv("squirrel_data_central_park.csv")
+
+# Get each Color on Primary Fur Color and count it.
+grey_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+cin_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+
+# Create a Squirrels Dictionary with Fur Color and Count for each.
+squirrels_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, cin_squirrels_count, black_squirrels_count]
+}
+
+# Create a Squirrels Data Frame with Squirrels Dictionary and Convert it to a new CSV File.
+squirrels_df = pandas.DataFrame(squirrels_dict)
+squirrels_df.to_csv("squirrels_count.csv")
 
 # ----------------------------------------------------------------------------------------------------------------------
