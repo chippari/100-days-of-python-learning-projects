ff02a3be6bc9bf305ff3e145eccc9f4c4634a487
Fabio Chippari <chipparidev@gmail.com>
2025-10-19 14:59:34 -0300

    refactor: deleted some blank lines.

--- day-26/04-nato-alphabet-project/main.py
+++ day-26/04-nato-alphabet-project/main.py
@@ -13,7 +13,6 @@ import pandas as pd
 # TODO-1: Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}.
 nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
-# print(nato_dict)
 
 # TODO-2: Create a list comprehension of the phonetic code words from a word that the user inputs.
 user_input = input("Enter a word: ").upper()
