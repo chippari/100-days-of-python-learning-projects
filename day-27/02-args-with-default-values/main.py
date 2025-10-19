a667b9289841c6781d43b97b83d2f3ae4af200a3
Fabio Chippari <chipparidev@gmail.com>
2025-10-19 16:49:18 -0300

    release: learned about args with default values.

--- day-27/02-args-with-default-values/main.py
+++ day-27/02-args-with-default-values/main.py
@@ -8,5 +8,14 @@
 # > Created: 2025-10-19
 # ----------------------------------------------------------------------------------------------------------------------
 
+# Create a function with Default Values
+def my_function(a = 1, b = 2, c = 3):
+    print(a, b, c)
+
+# Calling my_function without passing any values, so it will return with default values.
+my_function()
+
+# If you call my_function with some value, it will return with all set values or with some set values and default values.
+my_function(a = 10)
 
 # ----------------------------------------------------------------------------------------------------------------------
