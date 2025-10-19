a3262ee525f24f6ad2fd504367901f835251d884
Fabio Chippari <chipparidev@gmail.com>
2025-10-19 17:06:58 -0300

    release: learned about unlimited args.

--- day-27/03-unlimited-args/main.py
+++ day-27/03-unlimited-args/main.py
@@ -8,6 +8,11 @@
 # > Created: 2025-10-19
 # ----------------------------------------------------------------------------------------------------------------------
 
+# To create a Function with Unlimited Values, it needs to put the * symbol, and it's best practice to write "args".
+def add(*args):
+    return sum(args)
 
+# Calling Add Function with Unlimited Values.
+print(add(1,2,3,4))
 
 # ----------------------------------------------------------------------------------------------------------------------
