9708ecaf6c550b6a6f435dc6eeabeef7f9ee1703
Fabio Chippari <chipparidev@gmail.com>
2025-10-19 14:58:06 -0300

    release: completed nato alphabet project

--- day-26/04-nato-alphabet-project/main.py
+++ day-26/04-nato-alphabet-project/main.py
@@ -8,9 +8,16 @@
 # > Created: 2025-10-19
 # ----------------------------------------------------------------------------------------------------------------------
 
-# TODO-1: Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
+import pandas as pd
+
+# TODO-1: Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}.
+nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+# print(nato_dict)
 
 # TODO-2: Create a list comprehension of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ").upper()
+nato_output = [nato_dict[letter] for letter in user_input]
+print(nato_output)
 
 # ----------------------------------------------------------------------------------------------------------------------
