ee1ced19de97755564b8dfa63a5efbc358bf5e97
Fabio Chippari <chipparidev@gmail.com>
2025-10-20 02:45:41 -0300

    init: learning about other tkinter widgets.

---
+++ day-27/05-tkinter-other-widgets/main.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 27 - Tkinter Other Widgets -------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: main.py
+# > Module: Main Module
+# > Description: Entry point of the application; initializes and runs the main logic.
+# > Author: Fabio Chippari
+# > Created: 2025-10-20
+# ----------------------------------------------------------------------------------------------------------------------
+# > Imports ------------------------------------------------------------------------------------------------------------
+
+from tkinter import *
+
+# > Functions ----------------------------------------------------------------------------------------------------------
+
+def button_clicked(label, entry):
+    new_text = entry.get()
+    label.config(text=new_text)
+
+# > Main ---------------------------------------------------------------------------------------------------------------
+
+def main():
+    # Setup Window.
+    window = Tk()
+    window.title("My First GUI Program")
+    window.minsize(width=500, height=300)
+
+    # Label Setup.
+    label = Label(text="I am a Label", font=("Times New Roman", 16, "bold"))
+    label.pack()
+
+    # Button Setup.
+    button = Button(text="Click Me", command=lambda: button_clicked(label, entry))
+    button.pack()
+
+    # Entry Setup.
+    entry = Entry(width=25)
+    entry.pack()
+
+    # Keep Window Showing on Screen. ATTENTION: It's need to be always on the end.
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+# ----------------------------------------------------------------------------------------------------------------------
