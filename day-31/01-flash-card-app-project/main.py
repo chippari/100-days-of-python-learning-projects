78e25bb6577c75d7a0175993f6bb962486c3ab41
Fabio Chippari <chipparidev@gmail.com>
2025-10-22 20:05:07 -0300

    feat: set up ui elements using tkinter.

--- day-31/01-flash-card-app-project/main.py
+++ day-31/01-flash-card-app-project/main.py
@@ -9,11 +9,18 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # > Imports ------------------------------------------------------------------------------------------------------------
 
+from tkinter import *
+from typing import Optional
+
 # > Constants / Configuration ------------------------------------------------------------------------------------------
 
+FONT = "Arial"
 BACKGROUND_COLOR = "#B1DDC6"
 
 # > Global Variables ---------------------------------------------------------------------------------------------------
+
+window: Optional[Tk] = None
+canvas: Optional[Canvas] = None
 
 # > Functions ----------------------------------------------------------------------------------------------------------
 
@@ -21,10 +28,46 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 def main():
     # > Calling Global Variables ---------------------------------------------------------------------------------------
+    global window, canvas
 
     # > UI Setup -------------------------------------------------------------------------------------------------------
-    pass
 
+    # Window Setup.
+    window = Tk()
+    window.title("Flash Card App")
+    window.minsize(900, 750)
+    window.config(background=BACKGROUND_COLOR, padx=50, pady=50)
+
+    # Window Grid Setup (Rows & Columns)
+    rows_and_columns = 2
+    for i in range(rows_and_columns):
+        window.rowconfigure(i, weight=1)
+        window.columnconfigure(i, weight=1)
+
+    # Canvas Setup.
+    canvas = Canvas(width=800, height=530, bg=BACKGROUND_COLOR, highlightthickness=0)
+    # Canvas Front Card Setup.
+    front_card_img = PhotoImage(file="images/card_front.png")
+    canvas.create_image(400, 265, image=front_card_img)
+    # Language Canvas Text Setup.
+    canvas.create_text(400, 150, text="Title", font=(FONT, 40, "italic"))
+    # Word Canvas Text Setup.
+    canvas.create_text(400, 265, text="word", font=(FONT, 60, "bold"))
+    # Canvas Grid Position Setup.
+    canvas.grid(row=0, column=0, columnspan=2)
+
+    # Wrong Button Setup.
+    wrong_img = PhotoImage(file="images/wrong.png")
+    wrong_button = Button(image=wrong_img, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, bd=0)
+    wrong_button.grid(row=1, column=0)
+
+    # Right Button Setup.
+    right_img = PhotoImage(file="images/right.png")
+    right_button = Button(image=right_img, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, bd=0)
+    right_button.grid(row=1, column=1)
+
+    # Window Mainloop.
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
