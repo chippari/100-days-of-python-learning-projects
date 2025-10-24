ce029e3655692eb4dc7d9bdc84e6589da7013006
Fabio Chippari <chipparidev@gmail.com>
2025-10-24 02:02:31 -0300

    feat: learned about how to send email using smtp.

--- day-32/01-automated-email-project/main.py
+++ day-32/01-automated-email-project/main.py
@@ -5,9 +5,11 @@
 # > Module: Main Module
 # > Description: Entry point of the application; initializes and runs the main logic.
 # > Author: Fabio Chippari
-# > Created: 2025-10-23
+# > Created: 2025-10-24
 # ----------------------------------------------------------------------------------------------------------------------
 # > Imports ------------------------------------------------------------------------------------------------------------
+
+import smtplib
 
 # > Constants / Configuration ------------------------------------------------------------------------------------------
 
@@ -18,7 +20,19 @@
 # > Main ---------------------------------------------------------------------------------------------------------------
 
 def main():
-    pass
+    # Get Your Email and Password.
+    my_email = "example@gmail.com"
+    my_password = "123456"
+
+    # Create a Connection using smtplib and put your SMTP Provider.
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        # Secure the Connection by using starttls that will encrypt your email content if anyone try to intercept.
+        connection.starttls()
+        # Login in to your email.
+        connection.login(user=my_email, password=my_password)
+        # Send your email to someone.
+        connection.sendmail(from_addr=my_email, to_addrs="someone@gmail.com",
+                            msg="Subject:Hello\n\nThis where you put your message!")
 
 if __name__ == '__main__':
     main()
