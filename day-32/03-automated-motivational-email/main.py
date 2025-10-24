82c2c0eaba216e67ccbc04c4f0812e95410f7d68
Fabio Chippari <chipparidev@gmail.com>
2025-10-24 10:40:04 -0300

    feat: completed automated motivational quote email.

--- day-32/03-automated-motivational-email/main.py
+++ day-32/03-automated-motivational-email/main.py
@@ -9,8 +9,15 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # > Imports ------------------------------------------------------------------------------------------------------------
 
+import pandas
+import random
 import smtplib
 import datetime as dt
+
+# > Constants / Configuration ------------------------------------------------------------------------------------------
+
+MY_EMAIL = "example@gmail.com"
+MY_PASSWORD = "12345"
 
 # > Main ---------------------------------------------------------------------------------------------------------------
 
@@ -20,6 +27,24 @@ def main():
     now = dt.datetime.now()
     # Get Current Weekday.
     weekday = now.weekday()
+    weekday_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    # Get Quote Data and Set to be like a list.
+    quote_data = pandas.read_csv("quotes.txt")
+    quote_list = quote_data.values
+    # Get Random Quote of the Day from the list.
+    quote = random.choice(quote_list)[0]
+
+    # If it's Friday send a motivational quote email.
+    if "Friday" == weekday_list[weekday]:
+        # Create a Connection using smtplib and put your SMTP Provider.
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            # Secure the Connection by using starttls that will encrypt your email content if anyone try to intercept.
+            connection.starttls()
+            # Login in to your email.
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            # Send your email to someone.
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject: Motivational Quote\n\n{quote}")
 
 if __name__ == '__main__':
     main()
