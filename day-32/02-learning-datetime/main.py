694c57d47fd961b9aa65f6fe650e834826c805c1
Fabio Chippari <chipparidev@gmail.com>
2025-10-24 02:22:41 -0300

    feat: learned about datetime module.

--- day-32/02-learning-datetime/main.py
+++ day-32/02-learning-datetime/main.py
@@ -9,12 +9,24 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # > Imports ------------------------------------------------------------------------------------------------------------
 
-import datetime
+import datetime as dt
 
 # > Main ---------------------------------------------------------------------------------------------------------------
 
 def main():
-    pass
+    # Get Current Datatime.
+    now = dt.datetime.now()
+    # Get Current Year.
+    year = now.year
+    # Get Current Moth.
+    month = now.month
+    # Get Current Week Day.
+    day_of_week = now.weekday()
+    print(f"Year: {year}, Month: {month}, Day of Week: {day_of_week}")
+
+    # Create and Get a specific Day of your choice, like your birthdate.
+    date_of_birth = dt.datetime(year=2010, month=10, day=10, hour=10)
+    print(f"Date of Birth: {date_of_birth}")
 
 if __name__ == '__main__':
     main()
