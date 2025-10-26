d673700caf82563d7cc9ad950749ad3cb76e1740
Fabio Chippari <chipparidev@gmail.com>
2025-10-26 15:04:58 -0300

    release: learned about API parameters.

--- day-33/03-api-parameters/main.py
+++ day-33/03-api-parameters/main.py
@@ -10,12 +10,37 @@
 # > Imports ------------------------------------------------------------------------------------------------------------
 
 import requests
-from datetime import datetime
+
+# > Constants / Configuration ------------------------------------------------------------------------------------------
+
+MY_LAT = -23.550520
+MY_LONG = -46.633308
 
 # > Main ---------------------------------------------------------------------------------------------------------------
 
 def main():
-    pass
+    # Set Parameters accordingly from API Endpoint.
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    # Get API Response from API Endpoint.
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    # Raise an exception if there is an error.
+    response.raise_for_status()
+
+    # Get Data from API Response.
+    data = response.json()
+
+    # Get Sunrise & Sunset from data.
+    sunrise_hour = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset_hour = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+    print(f"Sunrise Hour: {sunrise_hour}")
+    print(f"Sunset Hour: {sunset_hour}")
+
 
 if __name__ == '__main__':
     main()
