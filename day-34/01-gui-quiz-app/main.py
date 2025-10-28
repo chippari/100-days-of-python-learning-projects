6915273b455f0ce1fcc489a7cbc3d092429a61a6
Fabio Chippari <chipparidev@gmail.com>
2025-10-27 22:30:24 -0300

    init: day 34 gui quiz app project.

---
+++ day-34/01-gui-quiz-app/main.py
@@ -0,0 +1,37 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 34 - GUI Quiz App Project --------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: main.py
+# > Module: Main Module
+# > Description: Entry point of the application; initializes and runs the main logic.
+# > Author: Fabio Chippari
+# > Created: 2025-10-27
+# ----------------------------------------------------------------------------------------------------------------------
+# > Imports ------------------------------------------------------------------------------------------------------------
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# > Main ---------------------------------------------------------------------------------------------------------------
+
+def main():
+    question_bank = []
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+
+    quiz = QuizBrain(question_bank)
+
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print("You've completed the quiz")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+if __name__ == '__main__':
+    main()
+
+# ----------------------------------------------------------------------------------------------------------------------
