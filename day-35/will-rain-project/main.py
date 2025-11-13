d83c9f8928c470b77588559f2f72eb2f9a55c4e6
Fabio Chippari <chipparidev@gmail.com>
2025-11-12 22:55:50 -0300

    release: completed "will rain project".

--- day-35/will-rain-project/main.py
+++ day-35/will-rain-project/main.py
@@ -13,30 +13,45 @@ import requests
 
 # > Constants / Configuration ------------------------------------------------------------------------------------------
 
-OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+# To get API KEY go to https://openweathermap.org/
 API_KEY = ""
-MY_LAT = -23.550520
-MY_LON = -46.633308
+
+OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+MY_LAT = -21.4587581
+MY_LON = -54.3698517
 
 # > Main ---------------------------------------------------------------------------------------------------------------
 
 def main():
     # Set Parameters accordingly from API Endpoint.
-    parameters = {
+    weather_parameters = {
         "lat": MY_LAT,
         "lon": MY_LON,
         "appid": API_KEY,
+        "cnt": 4
     }
 
     # Get API Response from API Endpoint.
-    response = requests.get(url=OWN_ENDPOINT, params=parameters)
+    response = requests.get(url=OWN_ENDPOINT, params=weather_parameters)
 
     # Raise an exception if there is an error.
     response.raise_for_status()
 
     # Get Data from API Response.
-    data = response.json()
-    print(data)
+    weather_data = response.json()
+
+    for time in weather_data["list"]:
+        # Get Weather Condition Codes.
+        weather_code = time["weather"][0]["id"]
+        # Get Weather Condition.
+        weather_condition = time["weather"][0]["main"]
+        # Get Weather Hour.
+        weather_hour = time["dt_txt"].split(" ")[1].split(":")[0]
+
+        if weather_code < 700:
+            print(f"Bring your umbrella! The weather condition is {weather_condition} at {weather_hour} hours.")
+        else:
+            print(f"You don't have to worry at {weather_hour} hours, because the weather condition is {weather_condition}!")
 
 
 if __name__ == '__main__':
