f5dbba42a3a86f02b5cdd82295b1c12f471c10f7
Fabio Chippari <chipparidev@gmail.com>
2025-11-12 22:01:44 -0300

    init: will rain project.

---
+++ day-35/will-rain-project/main.py
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# > Day 35 - Learning about API Key ------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# > File: main.py
+# > Module: Main Module
+# > Description: Entry point of the application; initializes and runs the main logic.
+# > Author: Fabio Chippari
+# > Created: 2025-11-12
+# ----------------------------------------------------------------------------------------------------------------------
+# > Imports ------------------------------------------------------------------------------------------------------------
+
+import requests
+
+# > Constants / Configuration ------------------------------------------------------------------------------------------
+
+OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = ""
+MY_LAT = -23.550520
+MY_LON = -46.633308
+
+# > Main ---------------------------------------------------------------------------------------------------------------
+
+def main():
+    # Set Parameters accordingly from API Endpoint.
+    parameters = {
+        "lat": MY_LAT,
+        "lon": MY_LON,
+        "appid": API_KEY,
+    }
+
+    # Get API Response from API Endpoint.
+    response = requests.get(url=OWN_ENDPOINT, params=parameters)
+
+    # Raise an exception if there is an error.
+    response.raise_for_status()
+
+    # Get Data from API Response.
+    data = response.json()
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
+
+# ----------------------------------------------------------------------------------------------------------------------
